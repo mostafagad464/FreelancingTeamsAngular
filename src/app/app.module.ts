e447253bd53f6e6664c0f4f094e6f1eae63da863
Mostafa M. Gad <mostafagad464@gmail.com>
2022-07-08 13:14:03 +0200

    add home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './Layout/layout.module';
 
 import { UserProfileModule } from './_user/user-profile/user-profile.module';
 
 @NgModule({
-  declarations: [AppComponent
-
+  declarations: [
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     UserProfileModule,
-    HttpClientModule
+    HttpClientModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
