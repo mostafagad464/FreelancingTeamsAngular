5467fa6104ae4c25fcd0b12262102d8b693e234c
Mostafa M. Gad <mostafagad464@gmail.com>
2022-07-14 01:16:42 +0200

    handle teams

--- src/app/_models/account.ts
+++ src/app/_models/account.ts
@@ -3,13 +3,20 @@ import { User } from "./user";
 export class Account {
     constructor(
         public id: number,
-        public identityId:number |null,
+        public identityId:number,
         public email:string,
-        public username: string,
+        public userName: string,
         public password: string,
         public firstName:string,
         public lastName:string,
         public type:string,
-        public user:User |null
+        public user:User
+        
+        
+        
+        
+        
+
+
     ){}
 }
