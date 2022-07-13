c600cf1ee3aa7b18d19ee6d8a9cd81e26861d7fc
Mostafa M. Gad <mostafagad464@gmail.com>
2022-07-14 01:41:48 +0200

    account

--- src/app/_models/account.ts
+++ src/app/_models/account.ts
@@ -3,14 +3,14 @@ import { User } from "./user";
 export class Account {
     constructor(
         public id: number,
-        public identityId:number,
+        public identityId:number|null,
         public email:string,
         public userName: string,
         public password: string,
         public firstName:string,
         public lastName:string,
         public type:string,
-        public user:User
+        public user:User|null
         
         
         
