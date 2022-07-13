cda682ae9bdfb42d1774ba1111a0dbd4784ec583
Your Name <you@example.com>
2022-07-14 00:27:33 +0200

    بسم الله

--- src/app/_models/account.ts
+++ src/app/_models/account.ts
@@ -1,17 +1,22 @@
 import { User } from "./user";
 
 export class Account {
-
     constructor(
         public id: number,
-        public identityId:number | null,
+        public identityId:number,
         public email:string,
-        public username: string ,
+        public userName: string,
         public password: string,
         public firstName:string,
         public lastName:string,
         public type:string,
         public user:User
-//test
+
+//ya rab
+
+
+
+
+
     ){}
 }
