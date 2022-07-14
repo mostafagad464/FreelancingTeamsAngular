d2b0642d9fa94f570c620d906006493411db01d0
EsraaNouh <eng.esraanouh@gmail.com>
2022-07-14 19:09:23 +0200

    account

--- src/app/_models/account.ts
+++ src/app/_models/account.ts
@@ -3,14 +3,14 @@ import { User } from "./user";
 export class Account {
     constructor(
         public id: number,
-        public identityId:number | null,
+        public identityId:number,
         public email:string,
-        public username: string,
+        public userName: string,
         public password: string,
         public firstName:string,
         public lastName:string,
         public type:string,
-        public user:User | null
+        public user:User
         
         
         
