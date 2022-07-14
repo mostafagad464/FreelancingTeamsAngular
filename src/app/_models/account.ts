023c8f0393fff774796903268c81e39c9075bb09
Mostafa M. Gad <mostafagad464@gmail.com>
2022-07-14 02:09:54 +0200

    add account model

---
+++ src/app/_models/account.ts
@@ -0,0 +1,15 @@
+import { User } from "./user";
+
+export class Account {
+    constructor(
+        public id: number,
+        public identityId:number | null,
+        public email:string,
+        public username: string ,
+        public password: string,
+        public firstName:string,
+        public lastName:string,
+        public type:string,
+        public user:User | null
+    ){}
+}
